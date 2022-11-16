b850b393d1e2374f8f97fd287a74536f025ed143
Christopher Isidro <cisidro@chapman.edu>
2022-11-16 08:25:32 -0800

    Added Scoring Script
    <3

---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float playerScore;
+    public float maxPoints;
+
+    public string letterScore;
+    public string finalScore;
+    
+    //reference to UI 
+
+    void Start()
+    {
+        
+    }
+
+    public void addScore()
+    {
+        playerScore++;
+        //score UI update Score (player Score) 
+    }
+
+    public void addMaxPoints()
+    {
+        maxPoints++;
+    }
+
+    public string calculateResult()
+    {
+        if (playerScore / maxPoints >= 0.9f)
+        {
+            letterScore = "A";
+        }
+        else if (playerScore / maxPoints >= 0.8f)
+        {
+            letterScore = "B";
+        }
+        else if (playerScore / maxPoints >= 0.7f)
+        {
+            letterScore = "C";
+        }
+        else if (playerScore / maxPoints >= 0.6f)
+        {
+            letterScore = "D";
+        }
+        else if (playerScore / maxPoints >= 0.5f)
+        {
+            letterScore = "F";
+        }
+        
+        return letterScore;
+    }
+
+    public string getFinalScore()
+    {
+        finalScore = playerScore + "/" + maxPoints;
+        return finalScore;
+    }
+    
+}
