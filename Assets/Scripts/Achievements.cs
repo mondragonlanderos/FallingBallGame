54456087cc2923a9e4342b2b5314a65d9c2bca19
jasyoon <78561612+jasyoon@users.noreply.github.com>
2022-11-16 08:11:28 -0800

    Revert "Merge branch 'main' of https://github.com/mondragonlanderos/FallingBallGame"
    This reverts commit d99305d511adca9b653d353a720ceb033303d776, reversing
    changes made to 1053891a1bbcd1e76f081d7a8ac5df463472e87c.

--- Assets/Scripts/Achievements.cs
+++ Assets/Scripts/Achievements.cs
@@ -4,65 +4,15 @@ using UnityEngine;
 
 public class Achievements : MonoBehaviour
 {
-    private AchievementUIBehavior achievementUIBehavior;
-    private bool hasMoved = false;
-    private bool gotThreeObjects = false;
-    private bool gotAllObjects = false;
-    
     // Start is called before the first frame update
     void Start()
     {
-        achievementUIBehavior = GetComponent<AchievementUIBehavior>();
-        StartCoroutine(WaitToShowAchievements());
+        
     }
 
-    public void UnlockAchievements()
+    // Update is called once per frame
+    void Update()
     {
-        achievementUIBehavior.ChangeCheckTo(1, CheckAllObjectsAchievement());
-        achievementUIBehavior.ChangeCheckTo(2, CheckThreeObjectsAchievement());
-        achievementUIBehavior.ChangeCheckTo(3, CheckNoMovementAchievement());
-    }
-
-    //Will be invoked with InputHandler events. When Player moves, hasMoved gets set to true
-    public void CheckForMovement()
-    {
-        hasMoved = true;
-    }
-
-    //if this method gets invoked via an event, its variable gets set to true instead of false
-    public void CheckForThreeObjects()
-    {
-        gotThreeObjects = true;
-    }
-
-    public void CheckForAllObjects()
-    {
-        gotAllObjects = true;
-    }
-    
-    //if player has not moved, returns true to show achievement completed
-    private bool CheckNoMovementAchievement()
-    {
-        return hasMoved == false;
-    }
-    
-    //if player got all three objects, returns true to show achievement completed
-    private bool CheckThreeObjectsAchievement()
-    {
-        return gotThreeObjects == true;
-    }
-    
-    //if player got all objects, returns true to show achievement completed
-    private bool CheckAllObjectsAchievement()
-    {
-        return gotAllObjects == true;
-    }
-
-    //stand in for waiting for the end of game
-    private IEnumerator WaitToShowAchievements()
-    {
-        yield return new WaitForSeconds(3f);
-        UnlockAchievements();
-        print("Unlocked");
+        
     }
 }
