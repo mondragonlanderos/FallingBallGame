289dacd77a958c72448a6c00b36c691e6c7ae3fb
Briana C <89816537+brcraig@users.noreply.github.com>
2022-11-16 08:17:18 -0800

    added UI for scoring

---
+++ Assets/scoreUI.cs
@@ -0,0 +1,39 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+class scoreUI{
+  public TextMeshProUGUI score;
+  public TextMeshProUGUI result;
+  public TextMeshProUGUI grade;
+
+  public void updateScoreText(int scored, int total){
+    score.text = "Score: \n" + scored;
+  }
+
+  public void DisplayLetterGrade(int scored, int total, float letterGrade){
+    char letter;
+
+    if((letterGrade <= 100) && (letterGrade > 89)){
+			letter = 'A';
+		}
+		else if((letterGrade <= 89) && (letterGrade > 79)){
+			letter = 'B';
+		}
+		else if((letterGrade <= 79) && (letterGrade > 69)){
+			letter = 'C';
+		}
+		else if((letterGrade <= 69) && (letterGrade > 59)){
+			letter = 'D';
+		}
+		else{
+			letter = 'F';
+		}
+		result.text = "Final Score: " + scored + "/" + total + "\n";
+
+		grade.text = letter;
+  }
+}
